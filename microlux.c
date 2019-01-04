bf0dc5eb04325173a27d8eee721e5320e9582aed
Graham Edgecombe <gpe@grahamedgecombe.com>
2019-01-04 17:51:41 +0000

    Add CDC vendor commands

--- microlux.c
+++ microlux.c
@@ -2,12 +2,27 @@
 #include <eputils.h>
 #include <fx2macros.h>
 #include <setupdat.h>
+#include <stddef.h>
+#include <stdint.h>
 #include <string.h>
 
 #define SYNCDELAY SYNCDELAY4
 
+#define CDC_SET_LINE_CODING        0x20
+#define CDC_GET_LINE_CODING        0x21
+#define CDC_SET_CONTROL_LINE_STATE 0x22
+
+struct cdc_line_coding {
+    uint32_t baud;
+    uint8_t stop_bits;
+    uint8_t parity;
+    uint8_t data_bits;
+};
+
 static volatile __bit dosud = FALSE;
 static volatile __bit configured = FALSE;
+static struct cdc_line_coding cdc_line_coding;
+static uint16_t cdc_line_state;
 
 static void cdc_puts(const char *str) {
     /* wait for free space in EP6 */
@@ -78,6 +93,25 @@ BOOL handle_get_descriptor() {
 }
 
 BOOL handle_vendorcommand(BYTE cmd) {
+    size_t len = sizeof(cdc_line_coding);
+    switch (cmd) {
+        case CDC_SET_LINE_CODING:
+            while (EP0BCL < len);
+            memcpy(&cdc_line_coding, EP0BUF, len);
+            EP0BCL = 0;
+            return TRUE;
+        case CDC_GET_LINE_CODING:
+            SUDPTRCTL &= ~bmSDPAUTO;
+            EP0BCH = MSB(len);
+            EP0BCL = LSB(len);
+            SUDPTRH = MSB(&cdc_line_coding);
+            SUDPTRL = MSB(&cdc_line_coding);
+            SUDPTRCTL |= bmSDPAUTO;
+            return TRUE;
+        case CDC_SET_CONTROL_LINE_STATE:
+            cdc_line_state = SETUP_VALUE();
+            return TRUE;
+    }
     return FALSE;
 }
 
