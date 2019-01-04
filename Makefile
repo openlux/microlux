9ef388da1d1d72ac6a0fc0dde3c802308e0c824d
Graham Edgecombe <gpe@grahamedgecombe.com>
2019-01-04 13:36:41 +0000

    Initial commit

---
+++ Makefile
@@ -0,0 +1,7 @@
+FX2LIB=/home/gpe/code/fx2lib
+
+SOURCES=microlux.c
+A51_SOURCES=dscr.a51
+BASENAME=microlux
+
+include $(FX2LIB)/lib/fx2.mk
---
+++ microlux.c
@@ -0,0 +1,69 @@
+#include <autovector.h>
+#include <fx2macros.h>
+#include <setupdat.h>
+
+volatile __bit dosud = FALSE;
+
+void main(void) {
+    RENUMERATE_UNCOND();
+
+    SETCPUFREQ(CLK_48M);
+
+    USE_USB_INTS();
+
+    ENABLE_SUDAV();
+    ENABLE_USBRESET();
+    ENABLE_HISPEED();
+
+    EA = 1;
+
+    for (;;) {
+        if (dosud) {
+            dosud = FALSE;
+            handle_setupdata();
+        }
+    }
+}
+
+BOOL handle_get_descriptor() {
+    return FALSE;
+}
+
+BOOL handle_vendorcommand(BYTE cmd) {
+    return FALSE;
+}
+
+BYTE handle_get_configuration(void) {
+    return 1;
+}
+
+BOOL handle_set_configuration(BYTE cfg) {
+    return cfg == 1;
+}
+
+BOOL handle_get_interface(BYTE ifc, BYTE *alt_ifc) {
+    return FALSE;
+}
+
+BOOL handle_set_interface(BYTE ifc, BYTE alt_ifc) {
+    return FALSE;
+}
+
+void handle_reset_ep(BYTE ep) {
+
+}
+
+void sudav_isr(void) __interrupt SUDAV_ISR {
+    dosud = TRUE;
+    CLEAR_SUDAV();
+}
+
+void usbreset_isr(void) __interrupt USBRESET_ISR {
+    handle_hispeed(FALSE);
+    CLEAR_USBRESET();
+}
+
+void hispeed_isr(void) __interrupt HISPEED_ISR {
+    handle_hispeed(TRUE);
+    CLEAR_HISPEED();
+}
