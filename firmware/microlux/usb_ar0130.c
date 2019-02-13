4a9c794b6ebf1bc0cb206b88eb8adee289898f5a
Graham Edgecombe <gpe@grahamedgecombe.com>
2019-02-13 18:36:46 +0000

    Use switch in usb_ar0130_handle_command
    This is consistent with usb_cdc_handle_command.

--- firmware/microlux/usb_ar0130.c
+++ firmware/microlux/usb_ar0130.c
@@ -7,19 +7,17 @@
 #define AR0130_CTRL_EXPOSURE 0x80
 
 bool usb_ar0130_handle_command(uint8_t cmd) {
-    if (cmd == AR0130_CTRL_EXPOSURE) {
-        struct ar0130_exposure_config new_config;
-        size_t len = sizeof(new_config);
+    struct ar0130_exposure_config new_config;
+    size_t len = sizeof(new_config);
+    switch (cmd) {
+        case AR0130_CTRL_EXPOSURE:
+            EP0BCL = 0;
+            while (EP0BCL < len);
 
-        EP0BCL = 0;
-        while (EP0BCL < len);
+            memcpy(&new_config, EP0BUF, len);
 
-        memcpy(&new_config, EP0BUF, len);
-
-        ar0130_start_exposure(&new_config);
-
-        return true;
+            ar0130_start_exposure(&new_config);
+            return true;
     }
-
     return false;
 }
